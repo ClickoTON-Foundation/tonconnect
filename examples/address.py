82a371e392838cc05de0186dd2b9d7ab58a187e7
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2023-04-12 11:40:57 +0400

    Small fixes

---
+++ examples/address.py
@@ -0,0 +1,10 @@
+from tonconnect.connector import Connector
+
+
+connector = Connector('https://tonclick.online/ton-connect.json')
+url = connector.connect('tonkeeper', 'test')
+
+print(f'Universal connect url for Tonkeeper: {url}')
+
+address = connector.get_address()
+print(f'Successfuly connected {address}.')
